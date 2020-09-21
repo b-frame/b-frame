1145e90c80397f784ed9926d9ec2d8f7f378b33b
Trezy <tre@trezy.com>
2020-09-21 00:10:55 -0500

    docs(twitch-adapter): add types

---
+++ packages/twitch-adapter/src/index.d.ts
@@ -0,0 +1,14 @@
+export class TwitchAdapter {
+	constructor(
+		config: {
+			capabilities: string[],
+			channels: string[],
+			credentials: {
+				username: string,
+				token: string,
+			},
+			prefix: string | string[],
+			websocketURL: string,
+		}
+	)
+}
